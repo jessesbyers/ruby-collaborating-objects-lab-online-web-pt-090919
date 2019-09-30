64216a59b522399ee633d6978f117adeb90936f8
Jesse Byers <jesse.s.byers@gmail.com>
2019-09-30 20:49:32 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -21,8 +21,8 @@ class Song
 #still not passing
   def self.new_by_filename(filename)
     # artist_name= Artist.find_or_create_by_name(filename.split(" - ").to_a[0])
-    @name = filename.split(" - ").to_a[1]
-    @artist = filename.split(" - ").to_a[0]
+    name = filename.split(" - ").to_a[1]
+    artist = filename.split(" - ").to_a[0]
     Song.new(name)
 
   end
